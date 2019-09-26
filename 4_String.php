c51f99b4f7ec967e6b7a8fdb07ca6cc7640ad14d
Winiarska <54930798+Winiarska@users.noreply.github.com>
2019-09-26 09:32:04 +0200

    Add files via upload

---
+++ 4_String.php
@@ -0,0 +1,12 @@
+<?php
+//nl2br
+  $text = <<< T
+  zsk - Zespół
+  Szkół
+  Komunikacji
+T;
+echo "Przed wywołaniem funkcji nl2br:<br> $text<br><hr>";
+echo "Po wywołaniu funkcji nl2br:<br>";
+echo nl2br($text);
+
+ ?>
